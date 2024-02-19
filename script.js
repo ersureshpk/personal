592eafb4661f7a7db3a29d23bde01f208cf8fbf6
Er. Suresh Pokhrel <53055377+ersureshpk@users.noreply.github.com>
2024-02-19 11:10:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function toggleContent(sectionId) {
-    var section = document.getElementById(sectionId);
-    var sections = document.getElementsByClassName('content-section');
-
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-    }
-
-    section.style.display = 'block';
-}
 
